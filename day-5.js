82f0d06fd4cff59955453991c2cfddc1c8515158
IMKINGJS <52960320+IMKINGJS@users.noreply.github.com>
2022-12-05 04:56:25 +0100

    Updated the code

--- day-5.js
+++ day-5.js
@@ -19,8 +19,8 @@ import products from "./day5-data.js";
 
 function getSaleItems(data){
     
-    return data.filter(value1 => value1.type === "sweet").map((value2) => ({item: value2.item,
-     price: value2.price}) 
+    return data.filter(candy => candy.type === "sweet").map(product => ({item: product.item,
+     price: product.price}) 
         
  )};
  
