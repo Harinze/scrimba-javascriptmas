38413d35355db2b6abf54decb1b24e695ed67057
IMKINGJS <52960320+IMKINGJS@users.noreply.github.com>
2022-12-08 03:10:34 +0100

    Added validTime - Day 8 challenge

---
+++ day8-index.js
@@ -0,0 +1,9 @@
+function validTime(str) {
+    //  write code here.
+    str = str.split("");
+ return str[0] < 3 && str[1] < 5 && str[3] < 6 ? true : false
+}
+console.log(validTime("22:55"))
+console.log(validTime("31:22"))
+console.log(validTime("00:59"))
+console.log(validTime("26:59"))
