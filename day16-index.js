5b3a2ab573b1e8e4f3fb71f163d8289323c899dd
IMKINGJS <52960320+IMKINGJS@users.noreply.github.com>
2022-12-16 03:28:03 +0100

    Added code

--- day16-index.js
+++ day16-index.js
@@ -3,4 +3,16 @@ function insertDashes(arr) {
     const dashStr = arr.split("").join("-");
          return dashStr.split("- -").join(" ");
 }
+
+
+function insertDashes(arr) {
+    // write code here
+         arr = arr.split(" ");
+         const arrOfDash = [];
+         for(let i = 0; i < arr.length; i++){
+              arrOfDash.push(arr[i].split("").join("-"))
+         }
+            const dashedStr = arrOfDash.join(" ")
+            return dashedStr
+        }
 console.log(insertDashes("abe vvd"))
